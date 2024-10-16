7d2c4697468bc8ce372917951920398b63ae5ef5
Anubhav Singhal <anubhavsinghal0505@gmail.com>
2024-10-17 04:37:04 +0530

    Login Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Students from './components/Students';
 import Courses from './components/Courses';
 import Assignment from './components/Assignment';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <Login />
       <div style={{ display: 'flex', height: '100%', gap: '10px' }}>
         <Sidebar />
         <div style={{ marginLeft: '300px', flexGrow: 1, height: '100vh', overflowY: 'auto', padding: '20px' }}>
